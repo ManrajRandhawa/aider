8d16a42ecb4fb9f612c49b4855552e7bf66868e2
Manraj1999 <myemail.manraj@gmail.com>
2020-10-11 17:45:43 +0800

    Commit 11/10/2020

--- aider.sql
+++ aider.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2020 at 12:28 PM
+-- Generation Time: Oct 11, 2020 at 11:45 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -52,7 +52,8 @@ CREATE TABLE `aider_driver_team` (
 --
 
 INSERT INTO `aider_driver_team` (`ID`, `Team_Name`, `Team_Members`, `Team_Status`) VALUES
-(15, 'ABC', '1,2', 'ACTIVE');
+(19, 'Arsenal', '1,5', 'INACTIVE'),
+(20, 'Bro', '2,4', 'INACTIVE');
 
 -- --------------------------------------------------------
 
@@ -116,11 +117,15 @@ CREATE TABLE `aider_transaction_driver` (
 --
 
 INSERT INTO `aider_transaction_driver` (`ID`, `Customer_ID`, `Team_ID`, `Pickup_Location`, `Dropoff_Location`, `Transaction_Datetime`, `Price`, `Primary_Rider_Cut`, `Secondary_Rider_Cut`, `Status`, `Team_Denied_List`) VALUES
-(1, 7, 15, '155, Jalan Bangau, Taman Berkeley, 41150 Klang, Selangor, Malaysia', 'Klang Parade, Jalan Meru, Kawasan 17, Klang, Selangor, Malaysia', '2020-10-02 18:27:21', '8.00', '2.40', '1.60', 'COMPLETED', ''),
-(2, 7, 15, '155, Jalan Bangau, Taman Berkeley, 41150 Klang, Selangor, Malaysia', 'Klang Parade, Jalan Meru, Kawasan 17, Klang, Selangor, Malaysia', '2020-10-02 18:32:26', '8.00', '2.40', '1.60', 'COMPLETED', ''),
-(3, 7, 15, '155, Jalan Bangau, Taman Berkeley, 41150 Klang, Selangor, Malaysia', 'Klang Parade, Jalan Meru, Kawasan 17, Klang, Selangor, Malaysia', '2020-10-02 18:34:04', '8.00', '2.40', '1.60', 'COMPLETED', ''),
-(4, 7, 15, '155, Jalan Bangau, Taman Berkeley, 41150 Klang, Selangor, Malaysia', 'Klang Parade, Jalan Meru, Kawasan 17, Klang, Selangor, Malaysia', '2020-10-02 18:35:07', '8.00', '2.40', '1.60', 'COMPLETED', ''),
-(5, 7, 15, '161, Jalan Bangau, Taman Berkeley, 41150 Klang, Selangor, Malaysia', 'MSU, Seksyen 13, Shah Alam, Selangor, Malaysia', '2020-10-06 15:31:02', '26.15', '7.85', '5.23', 'HEADING_TO_DESTINATION', '');
+(1, 7, NULL, 'MSU, Seksyen 13, Shah Alam, Selangor, Malaysia', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', '2020-10-08 15:51:35', '24.50', '0.00', '0.00', 'FINDING-RIDER', '18,19'),
+(2, 7, NULL, '140, Jalan Tiara 3, Bandar Baru Klang, 41150 Klang, Selangor, Malaysia', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', '2020-10-08 15:57:46', '8.45', '0.00', '0.00', 'FINDING-RIDER', '18'),
+(3, 7, NULL, '140, Jalan Tiara 3, Bandar Baru Klang, 41150 Klang, Selangor, Malaysia', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', '2020-10-08 15:58:02', '8.45', '0.00', '0.00', 'FINDING-RIDER', '18'),
+(4, 7, NULL, '43, Jalan Dato Yusuf Shahbudin 20, Taman Sentosa, 41200 Klang, Selangor, Malaysia', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', '2020-10-10 13:49:25', '17.45', '0.00', '0.00', 'FINDING-RIDER', '18'),
+(5, 7, 18, '43, Jalan Dato Yusuf Shahbudin 20, Taman Sentosa, 41200 Klang, Selangor, Malaysia', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', '2020-10-10 13:50:16', '17.45', '5.24', '3.49', 'COMPLETED', ''),
+(6, 7, NULL, '43, Jalan Dato Yusuf Shahbudin 20, Taman Sentosa, 41200 Klang, Selangor, Malaysia', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', '2020-10-10 13:50:28', '17.45', '0.00', '0.00', 'FINDING-RIDER', '18'),
+(7, 7, NULL, '43, Jalan Dato Yusuf Shahbudin 20, Taman Sentosa, 41200 Klang, Selangor, Malaysia', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', '2020-10-10 13:51:03', '17.45', '0.00', '0.00', 'FINDING-RIDER', ''),
+(8, 7, NULL, '43, Jalan Dato Yusuf Shahbudin 20, Taman Sentosa, 41200 Klang, Selangor, Malaysia', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', '2020-10-10 13:52:24', '17.45', '0.00', '0.00', 'FINDING-RIDER', ''),
+(9, 7, NULL, '43, Jalan Dato Yusuf Shahbudin 20, Taman Sentosa, 41200 Klang, Selangor, Malaysia', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', '2020-10-10 13:55:53', '17.45', '0.00', '0.00', 'FINDING-RIDER', '');
 
 -- --------------------------------------------------------
 
@@ -197,13 +202,15 @@ CREATE TABLE `aider_transaction_sorting` (
 --
 
 INSERT INTO `aider_transaction_sorting` (`ID`, `Transaction_ID`, `Transaction_Type`, `Customer_ID`, `RT_ID`, `Transaction_Datetime`, `Transaction_Status`, `Rider_Denied_List`) VALUES
-(1, 1, 'DRIVER', 7, 15, '2020-10-02 18:27:21', 'COMPLETED', ''),
-(2, 2, 'DRIVER', 7, 15, '2020-10-02 18:32:26', 'COMPLETED', ''),
-(3, 3, 'DRIVER', 7, 15, '2020-10-02 18:34:04', 'COMPLETED', ''),
-(4, 4, 'DRIVER', 7, 15, '2020-10-02 18:35:07', 'COMPLETED', ''),
-(5, 1, 'PARCEL', 7, 1, '2020-10-02 18:36:07', 'COMPLETED', ''),
-(6, 2, 'PARCEL', 7, 1, '2020-10-02 18:37:30', 'COMPLETED', ''),
-(7, 5, 'DRIVER', 7, 15, '2020-10-06 15:31:02', 'HEADING_TO_DESTINATION', '');
+(1, 1, 'DRIVER', 7, NULL, '2020-10-08 15:51:35', 'FINDING-RIDER', '18,19'),
+(2, 2, 'DRIVER', 7, NULL, '2020-10-08 15:57:46', 'FINDING-RIDER', '18'),
+(3, 3, 'DRIVER', 7, NULL, '2020-10-08 15:58:02', 'FINDING-RIDER', '18'),
+(4, 4, 'DRIVER', 7, NULL, '2020-10-10 13:49:25', 'FINDING-RIDER', '18'),
+(5, 5, 'DRIVER', 7, 18, '2020-10-10 13:50:16', 'COMPLETED', ''),
+(6, 6, 'DRIVER', 7, NULL, '2020-10-10 13:50:28', 'FINDING-RIDER', '18'),
+(7, 7, 'DRIVER', 7, NULL, '2020-10-10 13:51:03', 'FINDING-RIDER', ''),
+(8, 8, 'DRIVER', 7, NULL, '2020-10-10 13:52:24', 'FINDING-RIDER', ''),
+(9, 9, 'DRIVER', 7, NULL, '2020-10-10 13:55:53', 'FINDING-RIDER', '');
 
 -- --------------------------------------------------------
 
@@ -247,8 +254,10 @@ CREATE TABLE `aider_user_customer` (
 --
 
 INSERT INTO `aider_user_customer` (`ID`, `Name`, `Email_Address`, `Password_Hash`, `Phone_Number`, `Credit`, `Home_Address`, `First_Login`) VALUES
-(7, 'Manraj Singh', 'myemail.manraj@gmail.com', '$2y$10$fUfhpXy73PsN/5kD7I2yYeOhTrHQJ8Z2SFhP/UAdjazFxjM92UHNq', '0173385550', '1739.15', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', 'NO'),
-(12, 'Test Account', 'kewab32872@qqmimpi.com', '$2y$10$IJpHXLh3G/HyEDe78tgBC.RQyntwFjAqZdnBRG2L6kN.XPnqCaeku', '0123456789', '0.00', '', 'NO');
+(7, 'Manraj Singh', 'myemail.manraj@gmail.com', '$2y$10$fUfhpXy73PsN/5kD7I2yYeOhTrHQJ8Z2SFhP/UAdjazFxjM92UHNq', '0173385550', '1704.80', '147, Jalan Bangau, Taman Berkeley, Klang, Selangor, Malaysia', 'NO'),
+(12, 'Test Account', 'kewab32872@qqmimpi.com', '$2y$10$IJpHXLh3G/HyEDe78tgBC.RQyntwFjAqZdnBRG2L6kN.XPnqCaeku', '0123456789', '0.00', '', 'NO'),
+(13, 'Test Man', 'test-zh1maa6k3@srv1.mail-tester.com', '$2y$10$hDVeBVwUbH.pptoTJDlOGOVTJii9D.AKz/7ZuU9fgtuC8DYw7iKbi', '0123456789', '0.00', '', 'YES'),
+(14, 'Karamveer Singh', 'myemail.karamveer@gmail.com', '$2y$10$kg9cBhjxp9AQAwc4GyjfnOGkCAdmow9waC/lQSP4BMAJCuxVn/jCq', '0173055030', '0.00', '', 'YES');
 
 -- --------------------------------------------------------
 
@@ -287,18 +296,23 @@ CREATE TABLE `aider_user_rider` (
   `Transaction_Type` varchar(16) NOT NULL,
   `Transaction_ID` int(16) NOT NULL,
   `First_Login` varchar(16) NOT NULL,
-  `Wallet_Balance` decimal(16,2) NOT NULL
+  `Wallet_Balance` decimal(16,2) NOT NULL,
+  `Withdraw_Amount` decimal(16,2) NOT NULL DEFAULT 0.00,
+  `Total_Earnings` decimal(16,2) NOT NULL DEFAULT 0.00,
+  `Bank` varchar(32) DEFAULT NULL,
+  `Account_Number` int(32) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `aider_user_rider`
 --
 
-INSERT INTO `aider_user_rider` (`ID`, `Name`, `Email_Address`, `Password_Hash`, `Phone_Number`, `Vehicle_Model`, `Vehicle_Plate_Number`, `Loc_LNG`, `Loc_LAT`, `Team_ID`, `Rider_Type`, `Approval_Status`, `Status`, `Transaction_Type`, `Transaction_ID`, `First_Login`, `Wallet_Balance`) VALUES
-(1, 'Manraj Singh', 'myemail.manraj@gmail.com', '$2y$10$AsABq.fr50cvWHpsY3XWGuClALDXSGe6osUz2A3RlH3Y3U6EcOpFS', '0173385550', 'BMW 5-Series', 'BMW1234', '101.467566', '3.060067', 15, 'RIDER', 'APPROVED', 'HEADING_TO_DESTINATION', 'DRIVER', 5, 'NO', '27.25'),
-(2, 'Test', 'test@test.com', '$2y$10$AsABq.fr50cvWHpsY3XWGuClALDXSGe6osUz2A3RlH3Y3U6EcOpFS', '0173385550', 'BMW 5 Series (Test)', 'TEST1234', '', '', 15, 'RIDER', 'APPROVED', 'HEADING_TO_DESTINATION', 'DRIVER', 5, 'NO', '11.63'),
-(3, 'Test #2', 'mifot18998@mimpi99.com', '$2y$10$NnJm1bPw6l9DxCqTqRQkkONCpBDjUAxZKeV7oTYCE2uO1ECyWxDWS', '0123456789', 'BMW 7-Series', 'LOL3213', '', '', 0, 'DRIVER', 'APPROVED', 'ACTIVE', '', 0, 'YES', '0.00'),
-(4, 'George Chiew', 'kewage5302@elesb.net', '$2y$10$39Mde2wFuuL0EwL6TQke1.F7wivrZryh5AP1SjRsosV3iR7h.aO1i', '0123456789', 'Honda 80 CC', 'ABC123', '101.4671419', '3.0601361', 0, 'RIDER', 'APPROVED', 'ACTIVE', '', 0, 'NO', '0.00');
+INSERT INTO `aider_user_rider` (`ID`, `Name`, `Email_Address`, `Password_Hash`, `Phone_Number`, `Vehicle_Model`, `Vehicle_Plate_Number`, `Loc_LNG`, `Loc_LAT`, `Team_ID`, `Rider_Type`, `Approval_Status`, `Status`, `Transaction_Type`, `Transaction_ID`, `First_Login`, `Wallet_Balance`, `Withdraw_Amount`, `Total_Earnings`, `Bank`, `Account_Number`) VALUES
+(1, 'Manraj Singh', 'myemail.manraj@gmail.com', '$2y$10$AsABq.fr50cvWHpsY3XWGuClALDXSGe6osUz2A3RlH3Y3U6EcOpFS', '0173385550', 'BMW 5-Series', 'BMW1234', '', '', 19, 'RIDER', 'APPROVED', 'INACTIVE', '', 0, 'NO', '90.00', '0.00', '82.81', 'RHB', 1234567),
+(2, 'Test', 'test@test.com', '$2y$10$AsABq.fr50cvWHpsY3XWGuClALDXSGe6osUz2A3RlH3Y3U6EcOpFS', '0173385550', 'BMW 5 Series (Test)', 'TEST1234', '', '', 20, 'RIDER', 'APPROVED', 'ACTIVE', '', 0, 'NO', '28.64', '0.00', '0.00', NULL, NULL),
+(3, 'Test #2', 'mifot18998@mimpi99.com', '$2y$10$NnJm1bPw6l9DxCqTqRQkkONCpBDjUAxZKeV7oTYCE2uO1ECyWxDWS', '0123456789', 'BMW 7-Series', 'LOL3213', '', '', 0, 'DRIVER', 'APPROVED', 'ACTIVE', '', 0, 'YES', '0.00', '0.00', '0.00', NULL, NULL),
+(4, 'George Chiew', 'kewage5302@elesb.net', '$2y$10$39Mde2wFuuL0EwL6TQke1.F7wivrZryh5AP1SjRsosV3iR7h.aO1i', '0123456789', 'Honda 80 CC', 'ABC123', '101.4671419', '3.0601361', 20, 'RIDER', 'APPROVED', 'ACTIVE', '', 0, 'NO', '0.00', '0.00', '0.00', NULL, NULL),
+(5, 'Rajeshpal Singh', 'official.rajeshpal@gmail.com', '$2y$10$HZjZlQAhg10vtKND3ZGps.5eYWldwbyU8wKNtRsrM300QT6Dp.Tom', '0166800550', 'Proton X70', 'BAM1234', '', '', 19, 'DRIVER', 'APPROVED', 'ACTIVE', '', 0, 'YES', '0.00', '0.00', '0.00', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -378,7 +392,7 @@ ALTER TABLE `aider_user_rider`
 -- AUTO_INCREMENT for table `aider_driver_team`
 --
 ALTER TABLE `aider_driver_team`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `aider_merchant_product`
@@ -396,7 +410,7 @@ ALTER TABLE `aider_settings`
 -- AUTO_INCREMENT for table `aider_transaction_driver`
 --
 ALTER TABLE `aider_transaction_driver`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `aider_transaction_food`
@@ -414,7 +428,7 @@ ALTER TABLE `aider_transaction_parcel`
 -- AUTO_INCREMENT for table `aider_transaction_sorting`
 --
 ALTER TABLE `aider_transaction_sorting`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `aider_user_admin`
@@ -426,7 +440,7 @@ ALTER TABLE `aider_user_admin`
 -- AUTO_INCREMENT for table `aider_user_customer`
 --
 ALTER TABLE `aider_user_customer`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `aider_user_merchant`
@@ -438,7 +452,7 @@ ALTER TABLE `aider_user_merchant`
 -- AUTO_INCREMENT for table `aider_user_rider`
 --
 ALTER TABLE `aider_user_rider`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
